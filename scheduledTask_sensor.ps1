08e8cb94837f47d7411cbec54df2253432cd7ed7
s1lvxn <108528450+s1lvxn@users.noreply.github.com>
2025-05-12 14:29:25 +0200

    Update scheduledTask_sensor.ps1

--- scheduledTask_sensor.ps1
+++ scheduledTask_sensor.ps1
@@ -6,20 +6,12 @@
         Scheduled Task Sensor 
 
         .DESCRIPTION
-        Das Script prüft den Status von Scheduled Tasks auf einem remote Server. 
+        Das Script prÃ¼ft den Status von Scheduled Tasks auf einem remote Server. 
         
         Parameter:
         -server %host
         -taskname = Task Name
-
-        XML Values to configure the sensor
-        Channel: State
-            0 = Unknown
-            1 = Disabled
-            2 = Queued
-            3 = Ready
-            4 = Runing
-            
+        
         Channel: Last Result
             0 = Completed successfully
             1 = Currently running
@@ -35,7 +27,7 @@ param (
 )
 
 
-$taskinfo = Invoke-Command -ComputerName $Server -ScriptBlock { ‘{0:x}’ -f (Get-ScheduledTaskInfo -TaskName $using:taskname).LastTaskResult }
+$taskinfo = Invoke-Command -ComputerName $Server -ScriptBlock { â€˜{0:x}â€™ -f (Get-ScheduledTaskInfo -TaskName $using:taskname).LastTaskResult }
 
 switch($taskinfo){
     "0" {$lastresult = "0"}
